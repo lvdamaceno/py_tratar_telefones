db0a1a92e1b5f86bdbfd356bf6ece9cd864c45a9
lvdamaceno <lvdamaceno@gmail.com>
2022-11-23 12:45:20 -0300

    melhora no código

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv('contatos.csv')
-# removendo espaços dos telefones
-df['FONE1'] = df['FONE1'].str.replace(" ","")
-df['FONE2'] = df['FONE2'].str.replace(" ","")
-# inserindo prefixo do país
-df['FONE1'] = '+55' + df['FONE1'].astype(str)
-df['FONE2'] = '+55' + df['FONE2'].astype(str)
 
-df.to_csv('contatos_tratados.csv')
+def converte(arquivo):
+    df = pd.read_csv(arquivo)
+    # removendo espaços dos telefones
+    df['FONE1'] = df['FONE1'].str.replace(" ", "")
+    df['FONE2'] = df['FONE2'].str.replace(" ", "")
+    # inserindo prefixo do país
+    df['FONE1'] = '+55' + df['FONE1'].astype(str)
+    df['FONE2'] = '+55' + df['FONE2'].astype(str)
 
+    df.to_csv('contatos_tratados.csv')
+
+
+if __name__ == '__main__':
+    converte('contatos.csv')
